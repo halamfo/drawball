4501943deadde6bcd4ac1d4b9f8a44cef4d40bc9
halamfo <tungnt991@gmail.com>
2016-06-02 13:11:46 +0700

    Initial Commit

---
+++ ve den/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ve den
+//
+//  Created by Dân Tơi on 6/2/16.
+//  Copyright © 2016 Dân Tơi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
