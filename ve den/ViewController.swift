026003406244300232db570591881b557d065036
halamfo <tungnt991@gmail.com>
2016-06-03 15:26:00 +0700

    commit1

--- ve den/ViewController.swift
+++ ve den/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtsobong: UITextField!
+    @IBAction func btndraw(sender: AnyObject) {
+        for view in self.view.subviews {
+            if view is UIImageView{
+            view.removeFromSuperview()
+            }
+        }
+        let sobong : Int = Int(txtsobong.text!)!
+        vebong(sobong)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func vebong(sobong:Int){
+        for indexx in 0...sobong-1{
+            for indexy in 0...sobong-1{
+                let image = UIImage(named: "green")
+                let ball = UIImageView(image: image)
+                ball.center = CGPointMake(20 + CGFloat(indexx) * spacex(sobong), CGFloat(indexy) * spacey(sobong)+80)
+                self.view.addSubview(ball)
+            }
+        }
     }
+    func spacex(sobong:Int) ->CGFloat {
+        let space = ((self.view.bounds.size.width - 40))/CGFloat(sobong-1)
+        return space
+    }
+    func spacey(sobong:Int) ->CGFloat{
+        let space = ((self.view.bounds.size.height - 120))/CGFloat(sobong-1)
+        return space
 
-
+    }
 }
 
